9b296fae42bdbddab25a79530c802362abeb7c4f
Daniel7u7 <dg451546@gmail.com>
2024-01-18 22:29:01 -0600

    Pagina web Inventario

---
+++ vistas/login.php
@@ -0,0 +1,48 @@
+<div class="main-container">
+
+<form class="box login" action="" method="POST" autocomplete="off">
+    <h5 class="title is-5 has-text-centered is-uppercase">Sistema de inventario Tesco</h5>
+
+    <div class="field">
+        <label class="label">Usuario</label>
+        <div class="control">
+            <input class="input" type="text" name="login_usuario" pattern="[a-zA-Z0-9]{4,20}" maxlength="20" required>
+        </div>
+    </div>
+
+    <div class="field">
+        <label class="label">Clave</label>
+        <div class="control">
+            <input class="input" type="password" name="login_clave" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" required>
+        </div>
+    </div>
+
+    <!-- Nuevo campo "Tipo de Usuario" -->
+
+    <!--<div class="field">
+        <label class="label">Tipo de Usuario</label>
+        <div class="control">
+            <select class="select" name="login_tipo_usuario" required>
+                <option value="usuario">Usuario</option>
+                <option value="administrador">Administrador</option>
+                <option value="mantenimiento">Mantenimiento</option>
+            </select>
+        </div>
+    </div>
+-->
+
+    <p class="has-text-centered mb-4 mt-3">
+        <button type="submit" class="button is-info is-rounded">Iniciar sesión</button>
+    </p> 
+
+    <?php
+        if(isset($_POST['login_usuario']) && isset($_POST['login_clave'])){
+            require_once "./php/main.php";
+            require_once "./php/iniciar_sesion.php";
+        }
+    ?>
+</form>
+
+
+
+</div>
